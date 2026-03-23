ca1d63163ec29cbe5baa8947a2fa1ab5a4b0ac51
cjmot <cjmotyer@gmail.com>
2026-03-22 19:43:04 -0600

    Implement and deploy getFollowees lambda and endpoint

---
+++ tweeter-server/src/model/service/FollowService.ts
@@ -0,0 +1,33 @@
+import { FakeData, User, UserDto } from 'tweeter-shared';
+
+export class FollowService {
+    public async loadMoreFollowees(
+        authToken: string,
+        userAlias: string,
+        pageSize: number,
+        lastItem: UserDto | null
+    ): Promise<[UserDto[], boolean]> {
+        // TODO: Replace with the result of calling server
+        return this.getFakeData(lastItem, pageSize, userAlias);
+    }
+
+    public async loadMoreFollowers(
+        authToken: string,
+        userAlias: string,
+        pageSize: number,
+        lastItem: UserDto | null
+    ): Promise<[UserDto[], boolean]> {
+        // TODO: Replace with the result of calling server
+        return this.getFakeData(lastItem, pageSize, userAlias);
+    }
+
+    private async getFakeData(
+        lastItem: UserDto | null,
+        pageSize: number,
+        userAlias: string
+    ): Promise<[UserDto[], boolean]> {
+        const [users, hasMore] = FakeData.instance.getPageOfUsers(User.fromDto(lastItem), pageSize, userAlias);
+        const dtos = users.map((user) => user.dto);
+        return [dtos, hasMore];
+    }
+}
